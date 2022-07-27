9cee5b90134b87d2ecfc206bcea95c9a201f94fb
rreone2017 <rreone2017@gmail.com>
2022-07-27 14:30:15 +0900

    apply lombok

--- src/main/java/com/ll/exam/dto/ArticleDto.java
+++ src/main/java/com/ll/exam/dto/ArticleDto.java
@@ -1,50 +1,26 @@
 package com.ll.exam.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * ArticleDto class 역할
  * 1. 라우팅(교통정리)하기 위함.
  */
+
+@ToString
+@AllArgsConstructor
 public class ArticleDto {
+
+    @Getter @Setter
     private long id;
+
+    @Getter @Setter
     private String title;
+
+    @Getter @Setter
     private String body;
 
-    @Override
-    public String toString() {
-        return "ArticleDto{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
-                '}';
-    }
-
-    public ArticleDto(long id, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
 }
