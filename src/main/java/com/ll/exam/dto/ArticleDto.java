82b5d0bababd04540473e7ceb28708f8231dd720
rreone2017 <rreone2017@gmail.com>
2022-07-27 14:35:45 +0900

    apply @Data annotation in ArticleDto class

--- src/main/java/com/ll/exam/dto/ArticleDto.java
+++ src/main/java/com/ll/exam/dto/ArticleDto.java
@@ -1,26 +1,19 @@
 package com.ll.exam.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 /**
  * ArticleDto class 역할
  * 1. 라우팅(교통정리)하기 위함.
  */
 
-@ToString
+@Data
 @AllArgsConstructor
 public class ArticleDto {
 
-    @Getter @Setter
     private long id;
 
-    @Getter @Setter
     private String title;
-
-    @Getter @Setter
     private String body;
 
 }
